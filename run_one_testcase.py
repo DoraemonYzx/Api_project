676ab90152546eb476cf55a0fe6380d6b4abe4b7
DoraemonYzx <1353520146@qq.com>
2020-06-08 16:25:06 +0800

    新增运行某些用例的函数

---
+++ run_one_testcase.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author:余振新
+@file: run_one_testcase.py
+@time: 2020/06/08
+"""
+"""
+通过导入用例文件。
+使用suite.addTest函数可以单独运行某些用例。
+"""
+# import unittest
+# # from test_case import test_interface_select    用例类
+#
+#
+# # 构造测试套件
+# suite = unittest.TestSuite()
+# suite.addTest(test_interface_select.TestInterfaceSelect("test_01_categorySelect"))
+# suite.addTest(test_interface_select.TestInterfaceSelect("test_01_categorySelect_1"))   ddt用例写法
+#     # 执行测试
+# if __name__ == "__main__":
+#     runner = unittest.TextTestRunner(verbosity=2)
+#     runner.run(suite)
